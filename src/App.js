91a527b3b7146e8714b96b56e5115fec1717bb26
EC2 Default User <ec2-user@ip-172-31-41-129.ap-northeast-1.compute.internal>
2019-12-14 05:31:43 +0000

    complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Card, } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+import Header from "./Header";
+import Footer from "./Footer";
+import TabContents from "./TabContents";
+
+import * as Api from "./Api.js";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      news: []
+    };
+  }
+
+  async componentDidMount() {
+    let news = await Api.fetch();
+    this.setState({ 'news': news })
+  }
+
+  tabClick = async (eventKey) => {
+    let news = await Api.fetch(eventKey);
+    this.setState({ 'news': news })
+  }
+
+  render() {
+    const news = this.state.news.map((val, index) =>
+      <Col xs="12" sm="6" md="4" lg="3" key={index} >
+        <a href={val.url} target='blank'>
+          <Card className=''>
+            <Card.Img className='card-image' variant="top" src={val.urlToImage} />
+            <Card.Body>
+              <Card.Title className='card-title'>{val.title}</Card.Title>
+              <Card.Text className='card-text'>{val.description}</Card.Text>
+            </Card.Body>
+          </Card>
         </a>
-      </header>
-    </div>
-  );
+      </Col >
+    )
+    return (
+      <div className="App">
+        <Header />
+        <main className="main">
+          <TabContents news={news} tabClick={this.tabClick} />
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
