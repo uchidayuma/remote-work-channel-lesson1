7bb582466e156013c260249ae9458d7ae4ba43d2
EC2 Default User <ec2-user@ip-172-31-41-129.ap-northeast-1.compute.internal>
2020-01-01 04:52:14 +0000

    1-8

--- src/Api.js
+++ src/Api.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-export function fetch() {
+export function fetch(category=null) {
   return axios.get('https://newsapi.org/v2/top-headlines', {
     params: {
       // ここにクエリパラメータを指定する
       'country': 'jp',
       'apiKey': process.env.REACT_APP_NEWS_API_KEY,
+      'category': category,
     }
   }).then((results) => {
     return results.data.articles;
